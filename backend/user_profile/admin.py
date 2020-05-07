be58a6a77b9b32158982fac4e77b9349d2674194
avkuznetsov54 <avkuznetsov54@gmail.com>
2020-05-07 17:17:25 +0700

    list_filter for related field, create new user with related fields

--- backend/user_profile/admin.py
+++ backend/user_profile/admin.py
@@ -1,35 +1,82 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
+# from django.utils.html import mark_safe
+from django.utils.translation import gettext_lazy as _
 
-from .models import UserProfile
+from .models import Profile
 
 
-class UserProfileInline(admin.StackedInline):
-    model = UserProfile
+class CommercialFilter(admin.SimpleListFilter):
+    # https://docs.djangoproject.com/en/3.0/ref/contrib/admin/
+    title = 'Коммерция'
+    parameter_name = 'is_commercial'
+
+    def lookups(self, request, model_admin):
+
+        return (
+            ('True', _('Да')),
+            ('False', _('Нет')),
+        )
+
+    def queryset(self, request, queryset):
+
+        if self.value():
+            return queryset.filter(profile__is_commercial=self.value())
+        else:
+            return queryset
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
     can_delete = False
     verbose_name_plural = 'Доп. информация'
     # fields = ('is_residential',) # оставляет для отображения в админке
+    readonly_fields = ('get_image',)
+    fieldsets = (
+        (None, {
+            'fields': (
+                ('is_commercial', 'is_residential'),
+            ),
+        }),
+        (None, {
+            'fields': (
+                'full_name',
+                'phone_number',
+                'birth_date',
+            ),
+        }),
+        (None, {
+            'fields': (
+                'image', 'get_image',
+                'bio',
+            ),
+        }),
+    )
 
 
 class UserAdmin(BaseUserAdmin):
     model = User
     list_display = (
-                    'username',
-                    'get_full_name',
-                    'get_phone_number',
-                    'email',
-                    'get_is_commercia',
-                    'get_is_residential',
-                    # 'last_name',
-                    'is_staff',
-                    # 'is_superuser',
-                    'is_active',
+        'username',
+        'get_full_name',
+        'get_phone_number',
+        'email',
+        'get_is_commercial',
+        'get_is_residential',
+        # 'last_name',
+        'is_staff',
+        # 'is_superuser',
+        'is_active',
     )
-    # TODO: решить как использовать поля модели UserProfile в list_filter и search_fields модели User
+
+    # TODO: решить как использовать поля модели Profile в list_filter и search_fields модели User
+    # https://tproger.ru/translations/extending-django-user-model/#var2
     # list_filter = ('is_active',)
-    list_filter = ['is_active']
+    list_filter = ('is_active', CommercialFilter,)
     # search_fields = ('get_full_name',)
+
     # fieldsets = (
     #     (None, {'fields': ('username', 'password', 'email')}),
     #     # (('Personal info'), {
@@ -44,8 +91,9 @@ class UserAdmin(BaseUserAdmin):
     #         'fields': ('last_login', 'date_joined')
     #     }),
     # )
+
     list_editable = ['is_active']
-    inlines = (UserProfileInline,)
+    inlines = (ProfileInline,)
     save_on_top = True
     save_as = True
 
@@ -64,29 +112,29 @@ class UserAdmin(BaseUserAdmin):
         return () if request.user.is_superuser else ('is_staff', 'is_superuser', 'groups', 'user_permissions')
 
     def get_full_name(self, obj):
-        return obj.userprofile.full_name
+        return obj.profile.full_name
 
     get_full_name.short_description = 'ФИО'
-    get_full_name.admin_order_field = 'userprofile__full_name'
+    get_full_name.admin_order_field = 'profile__full_name'
 
     def get_phone_number(self, obj):
-        return obj.userprofile.phone_number
+        return obj.profile.phone_number
 
     get_phone_number.short_description = 'Телефон'
-    get_phone_number.admin_order_field = 'userprofile__phone_number'
+    get_phone_number.admin_order_field = 'profile__phone_number'
 
-    def get_is_commercia(self, obj):
-        return obj.userprofile.is_commercia
+    def get_is_commercial(self, obj):
+        return obj.profile.is_commercial
 
-    get_is_commercia.short_description = 'Коммерция'
-    get_is_commercia.admin_order_field = 'userprofile__is_commercia'
-    get_is_commercia.boolean = True
+    get_is_commercial.short_description = 'Коммерция'
+    get_is_commercial.admin_order_field = 'profile__is_commercial'
+    get_is_commercial.boolean = True
 
     def get_is_residential(self, obj):
-        return obj.userprofile.is_residential
+        return obj.profile.is_residential
 
     get_is_residential.short_description = 'Жилая'
-    get_is_residential.admin_order_field = 'userprofile__is_residential'
+    get_is_residential.admin_order_field = 'profile__is_residential'
     get_is_residential.boolean = True
 
 
--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -5,8 +5,10 @@ from django.dispatch import receiver
 
 import datetime
 
-
 # ф-ция генерит путь для загружаемого фото user
+from django.utils.safestring import mark_safe
+
+
 def generate_url_for_user_image(self, filename):
     now = datetime.datetime.now()
     url = 'images/users/%s/%s/%s/%s%s%s-%s-%s' % (now.year, now.month, now.day,
@@ -14,16 +16,18 @@ def generate_url_for_user_image(self, filename):
     return url
 
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='user')
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='user', related_name='profile')
 
-    is_commercia = models.BooleanField(default=False, verbose_name='Коммерческая')
+    is_commercial = models.BooleanField(default=False, verbose_name='Коммерческая')
     is_residential = models.BooleanField(default=False, verbose_name='Жилая')
 
     full_name = models.CharField(max_length=255, db_index=True, blank=True, verbose_name='ФИО')
 
     phone_number = models.CharField(max_length=20, null=True, blank=True, verbose_name='Телефон',
-                                    help_text='Без пробелов, скобок, +7 и 8. <p style="color:red;font-size:18px;">ТОЛЬКО ЦИФРЫ !!!</p>')
+                                    help_text='Без пробелов, скобок, +7 и 8. '
+                                              '<b style="color:red;font-size:10px;">ТОЛЬКО ЦИФРЫ !!!</b><br>'
+                                              'Пример: 9131112233')
 
     # location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
@@ -38,12 +42,25 @@ class UserProfile(models.Model):
     def __str__(self):
         return f'{self.user}'
 
+    def get_image(self):
+        return mark_safe(f'<img src={self.image.url} height="150"')
+
+    get_image.short_description = "Изображение"
+    get_image.allow_tags = True
+
+
+# если раскоментировать то будет ошибка при заполнении полей класса Profile при создании юзера из админки.
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
+#         # instance.profile.save()
+
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-        instance.profile.save()
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
 
 # TODO: дествия с useer model
 # @receiver(post_save, sender=User)
