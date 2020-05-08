923edfd0f51b64141ea212bd1a0a5254a2098517
avkuznetsov54 <avkuznetsov54@gmail.com>
2020-05-08 09:52:57 +0700

    kill problem 'User has no profile'

--- backend/user_profile/signals.py
+++ backend/user_profile/signals.py
@@ -1,29 +1,16 @@
 from django.contrib.auth.models import User
-from django.db import IntegrityError
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Profile
 
 
-# TODO: эта хреновина ведёт себя странно !!!
-# если раскоментировать то будет ошибка при заполнении полей класса Profile при создании юзера из админки.
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
 
-    print(created)  # allways True
-
-    if created:
-
-        Profile.objects.create(user=instance)
-
-
-        # users_without_profile = User.objects.filter(profile__isnull=True)
-        # for user in users_without_profile:
-        #     Profile.objects.create(user=user)
+    # TODO: магическое строчка кода,
+    #  которая решает проблему "User has no profile" при создании юзера с related model Profile,
+    #  с помощью OneToOneField
+    User.profile = property(lambda u: Profile.objects.get_or_create(user=u)[0])
 
     instance.profile.save()
 
-
-# @receiver(post_save, sender=User)
-# def save_user_profile(sender, instance, **kwargs):
-#     instance.profile.save()
